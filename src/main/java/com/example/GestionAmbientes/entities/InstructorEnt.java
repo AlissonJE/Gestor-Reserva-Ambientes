7a1832080db513f8d28b5821ae2e46b7be22ac95
AlissonJE <alissonjaramillo62@gmail.com>
2026-06-22 00:33:41 -0500

    Creacion Entities para instructor

---
+++ src/main/java/com/example/GestionAmbientes/entities/InstructorEnt.java
@@ -0,0 +1,42 @@
+package com.example.GestionAmbientes.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "instructores")
+public class InstructorEnt {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String nombre;
+    private String email;
+
+    public InstructorEnt() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
