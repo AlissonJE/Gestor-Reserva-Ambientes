00fce8f4f090ae9eac3f1d9f177a0761af052de5
AlissonJE <alissonjaramillo62@gmail.com>
2026-06-22 00:35:59 -0500

    Creacion de services para instructor

---
+++ src/main/java/com/example/GestionAmbientes/services/InstructorSer.java
@@ -0,0 +1,24 @@
+package com.example.GestionAmbientes.services;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.example.GestionAmbientes.entities.InstructorEnt;
+import com.example.GestionAmbientes.repositories.InstructorRep;
+
+@Service
+public class InstructorSer {
+
+    private final InstructorRep repository;
+
+    public InstructorSer(InstructorRep repository) {
+        this.repository = repository;
+    }
+
+    public List<InstructorEnt> obtenerTodos() {
+        return this.repository.findAll();
+    }
+
+    public InstructorEnt crear(InstructorEnt instructor) {
+        return this.repository.save(instructor);
+    }
+}
