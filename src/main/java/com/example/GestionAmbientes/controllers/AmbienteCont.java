b96bb8bd9b0f568140844c60589bdb786c87d576
AlissonJE <alissonjaramillo62@gmail.com>
2026-06-21 22:43:27 -0500

    Creacion controller para Ambiente

---
+++ src/main/java/com/example/GestionAmbientes/controllers/AmbienteCont.java
@@ -0,0 +1,29 @@
+package com.example.GestionAmbientes.controllers;
+
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.GestionAmbientes.entities.AmbienteEnt;
+import com.example.GestionAmbientes.services.AmbienteSer;
+
+@RestController
+@RequestMapping("/api/ambientes")
+public class AmbienteCont {
+
+    private final AmbienteSer service;
+
+    public AmbienteCont(AmbienteSer service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<AmbienteEnt> getAll() {
+        return this.service.obtenerTodos();
+    }
+
+    @PostMapping
+    public ResponseEntity<AmbienteEnt> create(@RequestBody AmbienteEnt ambiente) {
+        AmbienteEnt nuevoAmbiente = this.service.crear(ambiente);
+        return ResponseEntity.status(201).body(nuevoAmbiente);
+    }
+}
