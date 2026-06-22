db45b5a387c79337d049a286cb116b45ed358e57
AlissonJE <alissonjaramillo62@gmail.com>
2026-06-21 21:41:59 -0500

    Conexion a base de datos sql

---
+++ src/main/java/com/example/GestionAmbientes/DataBaseConnection.java
@@ -0,0 +1,13 @@
+package com.example.GestionAmbientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseConnection {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataBaseConnection.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/GestionAmbientes/GestionAmbientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionAmbientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAmbientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
