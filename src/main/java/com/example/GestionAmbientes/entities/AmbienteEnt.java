8444ce78b974f817c4d64deabd4b6722e8bb2773
AlissonJE <alissonjaramillo62@gmail.com>
2026-06-21 21:44:21 -0500

    Creacion entities para Ambiente

---
+++ src/main/java/com/example/GestionAmbientes/entities/AmbienteEnt.java
@@ -0,0 +1,32 @@
+package com.example.GestionAmbientes.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AmbienteEnt {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nombre;
+
+    @NotBlank
+    private String tipo;
+
+    @NotNull
+    @Min(1)
+    private Integer capacidad;
+
+    private Boolean activo = true;
+}
